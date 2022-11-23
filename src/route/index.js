92dbde0e1f4c5aec0247a77be1c77c10e8304098
muhireheir <muhire416@gmail.com>
2022-11-23 11:57:50 +0200

    add main branch

--- src/route/index.js
+++ src/route/index.js
@@ -1,21 +1,6 @@
 import {Router} from "express";
-import userRouter from "./userRoute";
-import OrderRouter from "./OrderRoute";
-import ShippingRouter from "./shippingRoute";
-import ProductRouter from "./productRoute";
-import TransactionRouter from "./transactionRoute";
-import CategoryRouter from "./categoryRoute";
-import passwordReset from "./passwordReset";
 import users from "./userRoute"
 const router = Router();
 router.use("/api/users",users);
-router.use("/api/password-reset",passwordReset);
-router.use('/user',userRouter);
-router.use('/ordering',OrderRouter);
-router.use('/Shipping', ShippingRouter)
-router.use('/product', ProductRouter)
-router.use('/transaction', TransactionRouter)
-router.use('/category',CategoryRouter)
-
 
 export default router;
--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -1,33 +1,6 @@
 
 
 import { Router }  from "express";
-import User,{Validate} from "../Model/userModel";
-import * as UserController from "../controllers/userController";
-import { checkUser,loginUser } from "../Middlewares/checkUserExist";
-import  TokenAuth from "../utils/token";
-// import { verifyUserToken } from "../Middlewares/verifyToken";
+
 const route = Router();
-route.post("/login", loginUser);
-
-  route.post("/",async (req,res)=>{
-    try {
-        const {error} = Validate(req.body);
-        if(error) return res.status(400).send(error.details[0].message);
-        const user = await new User(req.body).save();
-        const token = TokenAuth.generateToken({ user });
-        res.send({token:token,user})
-    } catch (error) {
-        res.send("An error occured"+error.message)
-        console.log(error)
-    }
-})  
-  .get(UserController.getAllController);
-
-
-route
-  .route("/:id")
-  .patch(UserController.updateController)
-  .get(UserController.getOneController)
-  .delete(UserController.deleteController);
-
 export default route;
