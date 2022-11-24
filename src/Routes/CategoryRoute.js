fc5ea360dcd18bb17860068452cc15ea35a77bb0
Auroreiyokwizerwa <auroreiyokwizerwa@gmail.com>
2022-11-24 05:32:11 -0700

    update and delete

--- src/Routes/CategoryRoute.js
+++ src/Routes/CategoryRoute.js
@@ -9,5 +9,8 @@ const CategoryRoute=express.Router();
 CategoryRoute.post("/",CategoryController.createCategory);
 
 CategoryRoute.get("/",CategoryController.getAllCategories);
+CategoryRoute.get("/:id",CategoryController.getOneCategory);
+CategoryRoute.put("/:id",CategoryController.updateAll);
+CategoryRoute.delete("/:id",CategoryController. deleteCategory);
 
-export default CategoryRoute;
+export default CategoryRoute; 
